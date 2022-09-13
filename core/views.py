f0a96ad9a88d1b320f31a31abf30966d32b778f4
kaybrian <kayonbrian@gmail.com>
2022-09-13 15:29:42 +0200

    added the number of likes 💯

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth 
 from django .contrib import messages
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 from django.contrib.auth.decorators import login_required
 
 
@@ -121,5 +121,21 @@ def upload(request):
 
 @login_required(login_url='login')
 def like_post(request):
-    pass
+    username = request.user.username 
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+    if like_filter == None: 
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes += 1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes -= 1
+        post.save()
+        return redirect('/')
 
